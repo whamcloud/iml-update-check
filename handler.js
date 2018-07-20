de59b1328e2f92734dbbb59bdd06399c8d82cad5
Joe Grund <jgrund@whamcloud.io>
2018-07-20 13:30:56 -0400

    Fix service dependencies
    The iml-update-handler does not account
    for the fact that it needs to be started after iml-settings-populator.
    
    Fix this, and refactor some code while we are here.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- handler.js
+++ handler.js
@@ -1,61 +1,77 @@
-const url = require('url');
-const fs = require('fs');
+const url = require("url");
+const fs = require("fs");
 
-const http = require('http');
-const https = require('https');
+const http = require("http");
+const https = require("https");
 
 const ca = fs.readFileSync(process.env.IML_CA_PATH);
 
-var port = process.env.LISTEN_PID > 0 ? { fd: 3 } : '/var/run/iml-update-handler.sock';
-http.createServer( (req, res) => {
-    var buff = "";
+const port =
+  process.env.LISTEN_PID > 0 ? { fd: 3 } : "/var/run/iml-update-handler.sock";
 
-    req.on('error', (err) => {
-	res.writeHead(400, {'Content-Type': 'application/javascript'});
-	res.end();
-	console.error("Request Failed: "+err);
+const urlObj = url.parse(
+  url.resolve(process.env.SERVER_HTTP_URL, "api/updates_available/")
+);
+
+const config = Object.assign(
+  {
+    headers: {
+      Authorization: `ApiKey ${process.env.API_USER}:${process.env.API_KEY}`,
+      Accept: "application/json",
+      "Content-type": "application/json"
+    },
+    ca: [ca],
+    requestCert: true,
+    method: "POST"
+  },
+  urlObj
+);
+
+http
+  .createServer((req, res) => {
+    let buff = "";
+
+    req.on("error", err => {
+      res.writeHead(400, { "Content-Type": "application/javascript" });
+      res.end();
+      console.error(`Request Failed: ${err}`);
     });
 
-    const hn = req.headers['x-forwarded-host'];
+    const hn = req.headers["x-forwarded-host"];
 
-    req.on('data', (d) => {
-	buff += d;
+    req.on("data", d => {
+      buff += d;
     });
 
-    req.on('end', () => {
-	var update = url.parse(url.resolve(process.env.SERVER_HTTP_URL, "api/updates_available/"));
-	update.headers = { "Authorization": 'ApiKey '+process.env.API_USER+':'+process.env.API_KEY,
-			   "Accept": "application/json",
-			   "Content-type": "application/json" };
-	update.ca = [ ca ];
-	update.requestCert = true;
-	update.method = 'POST';
+    req.on("end", () => {
+      let updatesAvail;
 
-	var updatesAvail;
-	try {
-	    updatesAvail = JSON.parse(buff);
-	} catch (e) {
-	    res.writeHead(400, {'Content-Type': 'application/javascript'});
-	    res.end();
-	    console.error("Bad Request: hn:"+hn+" data:`"+buff+"' : "+e);
-	    return;
-	}
+      try {
+        updatesAvail = JSON.parse(buff);
+      } catch (e) {
+        res.writeHead(400, { "Content-Type": "application/javascript" });
+        res.end();
+        console.error(`Bad Request: hn: ${hn} data: ${buff} : ${e}`);
+      }
 
-	var r = https.request(update);
-	r.on('error', (err) => {
-	    res.writeHead(500, {'Content-Type': 'application/javascript'});
-	    res.end();
-	    console.error("Failed to update ("+updatesAvail+") for "+hn+": "+err);
-	});
-	r.write(JSON.stringify({ host_address: hn,
-				 available: updatesAvail }));
-	r.end( () => {
-	    if (res.finished)
-		return;
-	    res.writeHead(200, {'Content-Type': 'application/javascript'});
-	    res.end();
-	});
+      const r = https.request(config);
 
+      r.on("error", err => {
+        res.writeHead(500, { "Content-Type": "application/javascript" });
+        res.end();
+        console.error(`Failed to update (${updatesAvail}) for ${hn}: ${err}`);
+      });
+      r.write(
+        JSON.stringify({
+          host_address: hn,
+          available: updatesAvail
+        })
+      );
+      r.end(() => {
+        if (res.finished) return;
+        res.writeHead(200, { "Content-Type": "application/javascript" });
+        res.end();
+      });
     });
-
-}).listen(port);
+  })
+  .listen(port);
